4571fede3d0e77aac27f88fba5f6c5fb50b2cb82
Premchandra Kumar <premchandra.kumar@meesho.com>
2022-12-13 21:11:24 +0530

    deleted user/pwd

--- src/main/java/com/meesho/notification/utils/kafka/KafkaConsumer.java
+++ src/main/java/com/meesho/notification/utils/kafka/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.meesho.notification.utils.kafka;
 
+import com.meesho.notification.models.entities.sms.SMSRequest;
+import com.meesho.notification.repositories.sms.SMSRequestRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -16,14 +18,26 @@ public class KafkaConsumer {
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
-    public KafkaConsumer() {
-
-    }
-
+    @Autowired
+    SMSRequestRepository smsRequestRepository;
 
     @KafkaListener(topics = SEND_SMS_TOPIC, groupId = SEND_SMS_CONSUMER_GROUP_ID)
     public void consumeMessage(String message) {
-        log.info("Received message on topic: " + SEND_SMS_TOPIC + "\n" + message  );
+        log.info("Received message on topic " + SEND_SMS_TOPIC + ": " + message);
+
+        log.info("Get the request details from db");
+        SMSRequest smsRequest =  smsRequestRepository.findById(Integer.valueOf(message)).get();
+
+        log.info("Check if number is blacklisted via redis");
+
+        log.info("Call the 3rd party api to send sms");
+
+        log.info("Update the status, message_id, failure_details if any in DB");
+        smsRequest.setStatus("SUCCESS");
+        smsRequest = smsRequestRepository.save(smsRequest);
+
+        log.info("Index the details in elastic search");
+
     }
 
 }
--- src/main/java/com/meesho/notification/utils/kafka/KafkaProducer.java
+++ src/main/java/com/meesho/notification/utils/kafka/KafkaProducer.java
@@ -11,15 +11,11 @@ public class KafkaProducer {
 
     private static final String SEND_SMS_TOPIC = "notification.send_sms";
 
-    public KafkaProducer() {
-
-    }
-
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
     public void sendMessage(String message) {
-        log.info("Published message on topic: " + SEND_SMS_TOPIC + "\n" + message  );
+        log.info("Published message on topic " + SEND_SMS_TOPIC + ": " + message  );
         kafkaTemplate.send(SEND_SMS_TOPIC, message);
     }
 
